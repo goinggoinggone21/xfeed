4ad5773801089e1ec6d3fc628b7efc25b1895270
Will Solomon <littlejewishboy01@yahoo.com>
2025-02-07 07:12:38 -0800

    Improving Main

--- main.py
+++ main.py
@@ -56,8 +56,8 @@ reddit = praw.Reddit(client_id=input_args[1], #REDDIT_CLIENT_ID
 					 username=input_args[5] #REDDIT_USER_NAME
 					 )
 twitter_api_authorized = Api(
-		access_token=input_args[6], #TWITTER_ACCESS_TOKEN,
-		access_secret=input_args[7], #TWITTER_ACCESS_TOKEN_SECRET
+		access_token=input_args[6], #TWITTER_ACCESS_TOKEN_BL,
+		access_secret=input_args[7], #TWITTER_ACCESS_TOKEN_SECRET_BL
 		client_id = '1712104648881192960',
 		consumer_key = input_args[8], #TWITTER_CONSUMER_KEY
 		consumer_secret = input_args[9], #TWITTER_CONSUMER_SECRET
